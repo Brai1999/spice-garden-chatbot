9b8ee7505407458d15c2cf01eafdc6f550d34f64
Brai1999 <br368315@gmail.com>
2026-05-18 21:20:44 +0530

    fix port

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,9 @@
 from groq import Groq
+import os
+from dotenv import load_dotenv
 
-client = Groq(api_key="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")
-
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 business_info = """
 You are a customer support assistant 
 for a restaurant called Spice Garden.
